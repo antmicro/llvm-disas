9259221aab4c9d5dfc4ed536fafb69d207d30353
Peter Gielda <pgielda@antmicro.com>
2019-11-24 14:32:12 +0100

    Little cleanup.

--- main.c
+++ main.c
@@ -27,23 +27,6 @@ typedef const char*(* LLVMSymbolLookupCallback) (void *DisInfo, uint64_t Referen
 
 EXTERNAL void* LLVMCreateDisasmCPUFeatures(const char *tripleName, const char* cpu, const char *features, void* disInfo, int tagType, LLVMOpInfoCallback getOpInfo, LLVMSymbolLookupCallback symbolLookUp);
 
-#define STUB { printf("WARNING: stub %s\n", __func__); }
-
-// 
-// TODO: those functions are just stubs
-// of zlib and curses functions.
-//
-
-EXTERNAL void setupterm() STUB;
-EXTERNAL void tigetnum() STUB;
-EXTERNAL void set_curterm() STUB;
-EXTERNAL void del_curterm() STUB;
-EXTERNAL void uncompress() STUB;
-EXTERNAL void crc32() STUB;
-EXTERNAL void compress2() STUB;
-EXTERNAL void compressBound() STUB;
-
-
 int opinfoCallback(void *DisInfo, uint64_t pc, uint64_t offset, uint64_t size, int tagType, void *tagBuf) {
 	printf("here %s\n", __func__);
 	return 1;
---
+++ stub.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define EXTERNAL extern "C"
+
+#define CSTUB(x) EXTERNAL void x() { printf("WARNING: stub %s\n", __func__); }
+
+// 
+// TODO: those functions are just stubs
+// of zlib and curses functions.
+//
+
+CSTUB(setupterm);
+CSTUB(tigetnum);
+CSTUB(set_curterm);
+CSTUB(del_curterm)
+CSTUB(uncompress);
+CSTUB(crc32);
+CSTUB(compress2);
+CSTUB(compressBound);
+
