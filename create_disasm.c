ef7dae4d1b67456281ddc2e696cb2993120e8b49
Adam Jeliński <ajelinski@antmicro.com>
2020-07-31 18:30:59 +0200

    [#17562] Add support for RISC-V m/a/f/d/c extensions
    The default RISC-V LLVM context supports only base instruction set 'i'.
    This commit adds a way to disassemble instructions from extensions.
    
    Additional features are enabled based on the CPU name. While creating a
    RISC-V CPU, if it's name starts with 'rv32' or 'rv64', then each further
    letter is passed as a feature to enable (e.g. '+X,+Y,' for 'rv32XY').
    
    'm', 'a', 'f', 'd' and 'c' features enable their corresponding RISC-V
    extensions (order isn't important). For 'g' all "basic" (all but 'c',
    i.e. compressed) extensions will be enabled. Therefore, RISC-V CPUs
    named 'rvXXmafdc' and 'rvXXgc' will have all five extensions enabled.
    
    'e' feature is also valid but it doesn't impact disassembling. Any other
    letter will be simply ignored by the LLVM with a warning. That's why 'i'
    is never passed - a valid part of a CPU name shouldn't cause a warning.

--- create_disasm.c
+++ create_disasm.c
@@ -1,11 +1,58 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "llvm-disas.h"
 
 void *LLVMCreateDisasmCPU(const char *tripleName, const char *cpu, void *disInfo, int tagType, void *getOpInfo, void *symbolLookUp);
 
+void *LLVMCreateDisasmCPUFeatures(const char *tripleName, const char *cpu, const char *features, void *disInfo, int tagType, void *getOpInfo, void *symbolLookUp);
+
+// Allow even more than possible unique features in case of some duplicates.
+#define MAX_FEATURES 100
+
+// Only used by the llvm_create_disasm_cpu; not in llvm-disas.h
+void *create_riscv_cpu(const char *tripleName, const char *cpu)
+{
+    // CPU name must begin with either "rv32" or "rv64" to pass features
+    const char *model = NULL;
+    if (strncmp(cpu, "rv32", 4) == 0) {
+        model = "generic-rv32";
+    } else if (strncmp(cpu, "rv64", 4) == 0) {
+        model = "generic-rv64";
+    } else {
+        return NULL;
+    }
+
+    char features[MAX_FEATURES * 3 + 1] = {0};
+    char *next_c = (char *)(cpu + 4);
+    int features_n = 0;
+    while (*next_c != '\0' && features_n < MAX_FEATURES) {
+        if (*next_c == 'g' && features_n <= MAX_FEATURES - 4) {
+            // "g" combines "m", "a", "f" and "d" features
+            strcat(features, "+m,+a,+f,+d,");
+            features_n += 4;
+        } else if (*next_c != 'i') { // Base integer instruction set is implicitly enabled
+            char feature[4];
+            // Invalid features, duplicates and extra commas are ignored gracefully.
+            snprintf(feature, 4, "+%c,", *next_c);
+            strcat(features, feature);
+            features_n++;
+        }
+        next_c++;
+    }
+
+    return LLVMCreateDisasmCPUFeatures(tripleName, model, features, NULL, 0, NULL, NULL);
+}
+
 void *llvm_create_disasm_cpu(const char *tripleName, const char *cpu)
 {
+    if (strncmp(tripleName, "riscv32", 7) == 0 || strncmp(tripleName, "riscv64", 7) == 0) {
+        void *ret_val = create_riscv_cpu(tripleName, cpu);
+        if (ret_val != NULL) {
+            return ret_val;
+        }
+    }
 
     return LLVMCreateDisasmCPU(tripleName, cpu, NULL, 0, NULL, NULL);
 }
