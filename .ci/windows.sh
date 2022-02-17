cd27e054fbad2a18d41e80a3a015a1fb5df7a6d3
Adam Jeliński <ajelinski@antmicro.com>
2022-02-17 13:38:04 +0100

    [#31629] CI: Windows: Use Python 3.6+ by default for `python`

--- .ci/windows.sh
+++ .ci/windows.sh
@@ -13,6 +13,24 @@ curl -O -L 'https://sourceforge.net/projects/mingw-w64/files/Toolchains%20target
 7z x 'x86_64-8.1.0-release-posix-sjlj-rt_v6-rev0.7z'
 PATH="$PWD/mingw64/bin:$PWD/cmake-3.17.3-win64-x64/bin:$PATH"
 
+# LLVM requires Python 3.6+. Let's test with 'f-strings' introduced in Python 3.6.
+if ! python -c 'f"test"'; then
+    # Add the first directory from 'Program Files' found with Python v3.6+ to PATH.
+    for _python_dir in "$(find '/cygdrive/c/Program Files' -name 'Python3*')"; do
+        if "$_python_dir/python.exe" -c 'f"test"'; then
+            export PATH="$_python_dir:$PATH"
+            break
+        fi
+    done
+
+    # Check if 'python' now calls at least Python 3.6.
+    if ! python -c 'f"test"'; then
+        echo "Python 3.6+ not found!"
+        exit 1
+    fi
+fi
+python --version
+
 # Configure, build and test
 mkdir shared
 cd shared
